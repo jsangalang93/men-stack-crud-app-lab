f25121f995d41519307b3cb0e7512e5dc8069d42
jsangalang93 <jsangalang93@gmail.com>
2024-04-25 18:57:35 -0400

    wip

--- server.js
+++ server.js
@@ -4,15 +4,39 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-const mongoose = require('mongoose');
+const mongoose = require ('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
 
+const Clothing = require('./models/clothing.js')
 
 app.use(express.urlencoded({extended:false}))
 
+app.get('/', async (req, res) => {
+    res.render('index.ejs')
+});
+
+app.get('/clothing', async (req, res) => {
+    const custClothes = await Clothing.find({})
+    res.send(custClothes)
+})
+
+//^add this to a show page and loop through things to print to a page.
+
+app.post('/clothing', async (req, res)=>{
+    console.log(req.body)
+    if(req.body.isReadyToWear === "on"){
+        req.body.isReadyToWear = true;
+    } else {
+        req.body.isReadyToWear = false;
+    }
+
+    const appendClothing = await Clothing.create(req.body)
+})
 
 
-
+app.get('/clothing/new', async (req, res) => {
+    res.render('clothing/new.ejs');
+})
 
 
 mongoose.connection.on('connected', ()=>{
